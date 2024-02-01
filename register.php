1ff07391acc58b20a64d0093ef57928382b2f2c7
nichakarnn21 <158503771+nichakarnn21@users.noreply.github.com>
2024-02-02 02:18:26 +0700

    Update register.php

--- register.php
+++ register.php
@@ -1,5 +1,5 @@
-<?php
-$mysqli = new mysqli("localhost", "root", "12345678", "library");
+//<?php
+//$mysqli = new mysqli("localhost", "root", "12345678", "library");
 
 if ($mysqli->connect_error) {
     die("การเชื่อมต่อฐานข้อมูลล้มเหลว: " . $mysqli->connect_error);
